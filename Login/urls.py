3aa1fbd1d0ae8bac642bdbd1be551ca768c88ba6
BISHESH-T <bisheshtandukar11@gmail.com>
2026-08-27 23:34:28 +0545

    Fix media url regex syntax error

--- Login/urls.py
+++ Login/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-    re_path(r'^media/(?path).*$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
