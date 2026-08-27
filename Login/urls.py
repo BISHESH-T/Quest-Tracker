dcf06d61b8b5013d513b8a3f5668dfa1fa9b97b5
BISHESH-T <bisheshtandukar11@gmail.com>
2026-08-27 23:29:19 +0545

    Update build script with media folder creation

--- Login/urls.py
+++ Login/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?path).*$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
